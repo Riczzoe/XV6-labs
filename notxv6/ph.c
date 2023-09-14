49b220f8189f84affecb0a4254c2724029dba62e
JohnFrankz <86246881+JohnFrankz@users.noreply.github.com>
2023-09-14 13:57:22 +0800

    Finish ph_safe ph_fast

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -7,6 +7,7 @@
 
 #define NBUCKET 5
 #define NKEYS 100000
+pthread_mutex_t lock;
 
 struct entry {
   int key;
@@ -52,7 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock);
   }
 
 }
@@ -98,6 +101,7 @@ get_thread(void *xa)
   return NULL;
 }
 
+
 int
 main(int argc, char *argv[])
 {
@@ -110,6 +114,7 @@ main(int argc, char *argv[])
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
+  pthread_mutex_init(&lock, NULL);
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
